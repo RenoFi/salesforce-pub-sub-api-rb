37d11063eccb61c871235cb8dcda73f220337223
João <joao@renofi.com>
2024-08-22 19:35:50 -0300

    Remove payload builder

--- example/payload_builder.rb
+++
@@ -1,7 +0,0 @@
-class PayloadBuilder
-  def self.build(decoded_event)
-    decoded_event["ChangeEventHeader"]["changeOrigin"] = "com/salesforce/api/soap/61.0;client=OurClient/"
-    decoded_event["Record_UUID__c"] = SecureRandom.uuid
-    decoded_event
-  end
-end
